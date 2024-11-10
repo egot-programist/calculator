ee6e29e3c4ea71c0943b2a5bc547445012e432bc
егор <pashalnik909@gmail.com>
2024-11-10 12:00:48 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Введите первое число:")
+a = float(input())
+print("Введите второе число:")
+b = float(input())
+
+sum = a + b
+print(str(a) + " + " + str(b) + " = " + str(sum))
+#class Sigmochad:
+    #def pashalko(Sigmochad):
+        #sigma = 100000000000012310000000000000000000000 * 12131240234920333333943294394304029839249034934994299090990238934890
+        #bruh = sigma * sigma + sigma.sas
+        #sigma.sas = " это много"
+        #print(bruh)
+#fife = Sigmochad
+#print(fife)
