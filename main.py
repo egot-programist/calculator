d093639249207742a130a888ed6957b15bf89bca
егор <pashalnik909@gmail.com>
2024-11-10 12:26:41 +0500

    Fixed Zero Division Bug

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ b = float(input())
 sum = a + b
 dif = a - b
 com = a * b
-pri = a / b
+
 print(str(a) + " + " + str(b) + " = " + str(sum))
 print(str(a) + " - " + str(b) + " = " + str(dif))
 print(str(a) + " * " + str(b) + " = " + str(com))
-print(str(a) + " / " + str(b) + " = " + str(pri))
+if b != 0:
+    pri = a / b
+    print(str(a) + " / " + str(b) + " = " + str(pri))
 #class Sigmochad:
     #def pashalko(Sigmochad):
         #sigma = 100000000000012310000000000000000000000 * 12131240234920333333943294394304029839249034934994299090990238934890
